d7474af19e64d5f6a4ba62635bacfce4b2825582
menuhwang <hmin0206@naver.com>
2022-07-08 10:57:25 +0900

    프로젝트 생성

---
+++ src/main/java/com/menu/wantyou/WantyouApplication.java
@@ -0,0 +1,13 @@
+package com.menu.wantyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantyouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menu/wantyou/WantyouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menu.wantyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
