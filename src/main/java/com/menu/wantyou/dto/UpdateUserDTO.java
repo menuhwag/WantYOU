81be6ce0fc712074d20f8a3d665d5d302fd0f3de
menuhwang <hmin0206@naver.com>
2022-07-19 16:00:48 +0900

    User 서비스 Update, Delete 추가

---
+++ src/main/java/com/menu/wantyou/dto/UpdateUserDTO.java
@@ -0,0 +1,16 @@
+package com.menu.wantyou.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateUserDTO {
+    private String password;
+    private String email;
+    private String nickname;
+}
