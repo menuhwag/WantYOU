cbb8145d153a53bb9bd26ac4dcd7860688075309
menuhwang <hmin0206@naver.com>
2022-07-21 18:49:46 +0900

    인증 메일 변경 기능 추가
    - 인증 메일을 잘못 입력한 경우 변경 기능 추가

--- src/main/java/com/menu/wantyou/repository/EmailVerifyTokenRepository.java
+++ src/main/java/com/menu/wantyou/repository/EmailVerifyTokenRepository.java
@@ -1,10 +1,14 @@
 package com.menu.wantyou.repository;
 
 import com.menu.wantyou.domain.EmailVerifyToken;
+import com.menu.wantyou.domain.User;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface EmailVerifyTokenRepository extends JpaRepository<EmailVerifyToken, Long> {
     Optional<EmailVerifyToken> findByToken(String token);
+    Optional<EmailVerifyToken> findByUser(User user);
+    boolean existsByUser(User user);
 }
