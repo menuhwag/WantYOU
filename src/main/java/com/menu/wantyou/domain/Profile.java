28461adfaec5fe6206b7a76b2a0517771b509c25
menuhwang <hmin0206@naver.com>
2022-07-26 18:40:27 +0900

    [Refactor] 도메인 refactoring

--- src/main/java/com/menu/wantyou/domain/Profile.java
+++ src/main/java/com/menu/wantyou/domain/Profile.java
@@ -1,5 +1,6 @@
 package com.menu.wantyou.domain;
 
+import com.menu.wantyou.dto.CreateProfileDTO;
 import com.menu.wantyou.dto.UpdateProfileDTO;
 import lombok.*;
 
@@ -9,7 +10,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @Entity
 public class Profile {
@@ -34,46 +34,83 @@ public class Profile {
     private String birthDay;
 
     @Column
-    private String hobby = "";
+    private String hobby;
 
-    public Profile(User user, String name, String year, String day) {
-        // 날짜 유효성 검사
-        validBirth(year, day);
+    public Profile(User user, CreateProfileDTO createProfileDTO) {
+        String birthYear = createProfileDTO.getBirthYear();
+        String birthDay = createProfileDTO.getBirthDay();
+        validBirth(birthYear, birthDay);
         this.user = user;
-        this.name = name;
-        this.birthYear = year;
-        this.birthDay = day;
+        this.name = createProfileDTO.getName();
+        this.birthYear = birthYear;
+        this.birthDay = birthDay;
+        this.hobby = createProfileDTO.getHobby();
     }
 
     public Profile update(UpdateProfileDTO updateProfileDTO) {
-        String name = updateProfileDTO.getName();
-        String year = updateProfileDTO.getYear();
-        String day = updateProfileDTO.getDay();
-        String hobby = updateProfileDTO.getHobby();
+        String birthYear = updateProfileDTO.getBirthYear();
+        String birthDay = updateProfileDTO.getBirthDay();
 
-        // 날짜 유효성 검사
-        if (year != null && day != null) validBirth(year, day);
+        validBirth(birthYear, birthDay);
 
-        if (name != null) setName(name);
-        if (year != null) setBirthYear(year);
-        if (day != null) setBirthDay(day);
-        if (hobby != null) setHobby(hobby);
+        updateName(updateProfileDTO.getName());
+        updateBirthYear(birthYear);
+        updateBirthDay(birthDay);
+        updateHobby(updateProfileDTO.getHobby());
 
         return this;
     }
+    private void updateName(String name) {
+        if (name != null) this.name = name;
+    }
+
+    private void updateBirthYear(String year) {
+        if (year != null) this.birthYear = year;
+    }
+
+    private void updateBirthDay(String day) {
+        if (day != null) this.birthDay = day;
+    }
+
+    private void updateHobby(String hobby) {
+        if (hobby != null) this.hobby = hobby;
+    }
 
     private void validBirth(String birthYear, String birthDay) throws IllegalArgumentException{
-        try {
-            String year = birthYear;
-            String month = birthDay.substring(0,2);
-            String day = birthDay.substring(2);
-            String date = year + "-" + month + "-" + day;
+        if (!existBirthYear(birthYear)) return;
+        if (!existBirthDay(birthDay)) return;
+        parseDate(toStringDate(birthYear, birthDay));
+    }
 
+    private boolean existBirthYear(String birthYear) {
+        return birthYear != null;
+    }
+
+    private boolean existBirthDay(String birthDay) {
+        return birthDay != null;
+    }
+
+    private void parseDate(String date) throws IllegalArgumentException {
+        try {
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
             format.setLenient(false);
             format.parse(date);
         } catch (ParseException e) {
             throw new IllegalArgumentException("유효하지 않은 생일입니다.");
         }
+    }
+
+    private String toStringDate(String birthYear, String birthDay) {
+        String month = parseMonth(birthDay);
+        String day = parseDay(birthDay);
+        return birthYear + "-" + month + "-" + day;
+    }
+
+    private String parseMonth(String birthDay) {
+        return birthDay.substring(0,2);
+    }
+
+    private String parseDay(String birthDay) {
+        return birthDay.substring(2);
     }
 }
