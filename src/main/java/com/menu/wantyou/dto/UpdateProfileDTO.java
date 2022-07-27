1e6d8e0bf3984e3a3e50a5b61f0a22776bdc1896
menuhwang <hmin0206@naver.com>
2022-07-27 13:32:37 +0900

    [Refactor] 어노테이션 수정

--- src/main/java/com/menu/wantyou/dto/UpdateProfileDTO.java
+++ src/main/java/com/menu/wantyou/dto/UpdateProfileDTO.java
@@ -3,10 +3,8 @@ package com.menu.wantyou.dto;
 import lombok.*;
 
 @Getter
-@Setter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class UpdateProfileDTO {
     private String name;
     private String birthYear;
