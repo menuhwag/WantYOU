4a6ab66ed11d032a33337cb962a7f5a3682e3e40
menuhwang <hmin0206@naver.com>
2022-07-29 10:45:28 +0900

    [Refactor] profile 도메인 수정

--- src/test/java/com/menu/wantyou/domain/ProfileTest.java
+++ src/test/java/com/menu/wantyou/domain/ProfileTest.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProfileTest {
-    private String username = "jack01";
-    private String password = "passw0rd";
-    private String email = "jack01@gmail.com";
-    private String nickname = "jackson";
-    private String name = "홍길동";
-    private String year = "2000";
-    private String day = "0315";
+    private final String username = "jack01";
+    private final String password = "passw0rd";
+    private final String email = "jack01@gmail.com";
+    private final String nickname = "jackson";
+    private final String name = "홍길동";
+    private final String year = "2000";
+    private final String day = "0315";
 
     User user = new User(username, password, email, nickname);
     @Test
@@ -23,10 +23,9 @@ class ProfileTest {
                                                             .birthYear(year)
                                                             .birthDay(day)
                                                             .build();
-        Profile profile = new Profile(user, createProfileDTO);
+        Profile profile = new Profile(createProfileDTO);
 
         assertNull(profile.getId());
-        assertEquals(user, profile.getUser());
         assertEquals(name, profile.getName());
         assertEquals(year, profile.getBirthYear());
         assertEquals(day, profile.getBirthDay());
@@ -51,8 +50,8 @@ class ProfileTest {
                                                             .birthDay(day2)
                                                             .build();
 
-        assertThrows(IllegalArgumentException.class, () -> new Profile(user, createProfileDTO1));
-        assertThrows(IllegalArgumentException.class, () -> new Profile(user, createProfileDTO2));
+        assertThrows(IllegalArgumentException.class, () -> new Profile(createProfileDTO1));
+        assertThrows(IllegalArgumentException.class, () -> new Profile(createProfileDTO2));
     }
 
     @Test
@@ -62,7 +61,7 @@ class ProfileTest {
                                                             .birthYear(year)
                                                             .birthDay(day)
                                                             .build();
-        Profile profile = new Profile(user, createProfileDTO);
+        Profile profile = new Profile(createProfileDTO);
         String newName = "김첨지";
         String newYear = "2001";
         String newDay = "1009";
@@ -78,7 +77,6 @@ class ProfileTest {
         Profile updateProfile =  profile.update(updateProfileDTO);
 
         assertEquals(profile.getId(), updateProfile.getId());
-        assertEquals(profile.getUser(), updateProfile.getUser());
         assertEquals(newName, updateProfile.getName());
         assertEquals(newYear, updateProfile.getBirthYear());
         assertEquals(newDay, updateProfile.getBirthDay());
@@ -92,7 +90,7 @@ class ProfileTest {
                                                             .birthYear(year)
                                                             .birthDay(day)
                                                             .build();
-        Profile profile = new Profile(user, createProfileDTO);
+        Profile profile = new Profile(createProfileDTO);
         String newYear = "2001";
         String newDay = "1309";
 
