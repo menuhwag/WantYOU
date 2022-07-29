494056bf927893d0216c41054b69915f8ff10a7b
menuhwang <hmin0206@naver.com>
2022-07-29 10:51:22 +0900

    [Feat] profile 조회, 업데이트 테스트작성

---
+++ src/test/java/com/menu/wantyou/repository/ProfileRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.menu.wantyou.repository;
+
+import com.menu.wantyou.domain.Profile;
+import com.menu.wantyou.dto.CreateProfileDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class ProfileRepositoryTest {
+    private final String name = "홍길동";
+    private final String birthYear = "1999";
+    private final String birthDay = "0516";
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    private Profile savedProfile;
+
+    @BeforeEach
+    public void setUp() {
+        CreateProfileDTO createProfileDTO = CreateProfileDTO.builder()
+                                                            .name(name)
+                                                            .birthYear(birthYear)
+                                                            .birthDay(birthDay)
+                                                            .build();
+
+        Profile profile = new Profile(createProfileDTO);
+        savedProfile = profileRepository.save(profile);
+    }
+
+    @Test
+    public void create_Profile() {
+        assertNotNull(savedProfile.getId());
+        assertEquals(name, savedProfile.getName());
+        assertEquals(birthYear, savedProfile.getBirthYear());
+        assertEquals(birthDay, savedProfile.getBirthDay());
+        assertEquals("", savedProfile.getHobby());
+    }
+
+    @Test
+    public void find_By_Id() {
+        Profile findProfile = profileRepository.findById(savedProfile.getId()).get();
+
+        assertNotNull(findProfile);
+        assertEquals(savedProfile, findProfile);
+    }
+
+    @Test
+    public void delete_Profile() {
+        Long savedProfileId = savedProfile.getId();
+
+        Profile profile = profileRepository.findById(savedProfileId).get();
+
+        profileRepository.delete(profile);
+
+        Optional<Profile> findProfile = profileRepository.findById(savedProfileId);
+        assertEquals(Optional.empty(), findProfile);
+    }
+}
