fcf361b6d73a37c92d69d007a29e1021e3f10ea2
menuhwang <hmin0206@naver.com>
2022-09-24 20:07:30 +0900

    [fix] DTO 수정
    - NoArgsConstructor 추가
    - AllArgsConstructor 추가

--- src/main/java/com/menu/wantyou/dto/SignUpDTO.java
+++ src/main/java/com/menu/wantyou/dto/SignUpDTO.java
@@ -2,11 +2,15 @@ package com.menu.wantyou.dto;
 
 import com.menu.wantyou.dto.profile.ProfileReqDTO;
 import com.menu.wantyou.dto.user.UserSignUpDTO;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SignUpDTO {
     private UserSignUpDTO user;
     private ProfileReqDTO profile;
--- src/main/java/com/menu/wantyou/dto/user/UserUpdateDTO.java
+++ src/main/java/com/menu/wantyou/dto/user/UserUpdateDTO.java
@@ -1,13 +1,17 @@
 package com.menu.wantyou.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserUpdateDTO {
     @Size(min = 8, max = 16)
     private String password;
